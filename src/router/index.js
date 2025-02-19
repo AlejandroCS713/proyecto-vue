0481f328c9e001772563c9e4b20a52b6673ba3f9
AlejandroCS713 <alejandro2003carbonell@gmail.com>
2025-02-19 11:04:21 +0100

    Instala Vee-Validate, Crea Modulos

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -11,12 +12,17 @@ const router = createRouter({
     {
       path: '/contador',
       name: 'contador',
-      component: () => import('../components/Contador.vue')
+      component: () => import('../modules/contador/components/Contador.vue')
     },
     {
 path: '/lista-de-tareas',
       name: 'ListaDeTareas',
-      component: () => import('../components/ListaDeTareas.vue')
+      component: () => import('../modules/listaDeTareas/components/ListaDeTareas.vue')
+    },
+    {
+      path: '/registrar',
+      name: 'registrar',
+      component: () => import('../modules/registro/views/RegistrarView.vue') 
     } 
   ]
 })
